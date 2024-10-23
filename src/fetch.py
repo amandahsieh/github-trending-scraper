dc493347efac00710b62b9b4d4a4bd1e9e3471fe
amandahsieh <amandahsieh.926188@gmail.com>
2024-10-23 15:11:38 +0800

    <refactor>[github trending scraper]: fetch

--- src/fetch.py
+++ src/fetch.py
@@ -2,34 +2,26 @@ from typing import Optional, List
 import requests
 from urllib.parse import quote as urlquote
 
-from .valid import (
-    valid_language
-)
-from .github_api import (API_REPOS)
+from .valid import is_valid_language
+from .github_api import API_REPOS
 
-desired_columns = ["author", "url", "stars", "forks", "language"]
+DESIRED_COLUMNS = ["author", "url", "stars", "forks", "language"]
 
-def fetch_repos(
-    period: str,
-    language: Optional[str] = "",
-) -> List[dict]:
-    if (
-        not isinstance(language, (str, type(None)))
-        or (language and not valid_language(language))
-    ): 
+def fetch_repos(period: str, language: Optional[str] = "") -> List[dict]:
+    if not isinstance(language, (str, type(None))) or (language and not is_valid_language(language)):
         raise ValueError(f"Invalid Language: {language}")
-    if (
-        not isinstance(period, (str, type(None)))
-        or (period and period not in ("daily", "weekly", "monthly"))
-    ):
+
+    if period not in ("daily", "weekly", "monthly"):
         raise ValueError(f"Invalid Period: {period}")
+
     language_param = urlquote(language, safe="+") if language else ""
-    url: str = f"{API_REPOS}?language={language_param}&since={period}"
+    url = f"{API_REPOS}?language={language_param}&since={period}"
 
     res = requests.get(url).json()
     repos = []
     for repo in res:
-        filtered_repo = {col: repo[col] for col in desired_columns if col in repo}
+        filtered_repo = {col: repo.get(col) for col in DESIRED_COLUMNS}
         filtered_repo["fullname"] = f"{repo['author']}/{repo['name']}"
         repos.append(filtered_repo)
+
     return repos
